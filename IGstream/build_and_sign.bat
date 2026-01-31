e816063cd9f5394cef011da07b5032a3c1272715
Vicky Kumar <imvickykumar999@gmail.com>
2026-01-31 13:29:50 +0530

    Signed_24x7Live_Instagram.exe

--- IGstream/build_and_sign.bat
+++ IGstream/build_and_sign.bat
@@ -5,28 +5,21 @@ setlocal
 set COMPANY_NAME=24x7LiveStream
 set PFX_PATH=C:\Users\surface\Documents\24x7.pfx
 set PFX_PASS=YourPassword123
-:: Update this path to your actual signtool location if different
+:: Note: Double-check your Windows SDK version folder (e.g., 10.0.22621.0)
 set SIGNTOOL_PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe"
 
-echo [1/4] Installing PyInstaller...
+echo [1/3] Preparing PyInstaller...
 pip install pyinstaller --quiet
 
-echo [2/4] Building Instagram Stream EXE...
-cd IGstream
-pyinstaller --onefile --windowed --icon=IGTV.jpg --name=24x7Live_Instagram InstagramLiveStreamFolder.py
-cd ..
+echo [2/3] Building Instagram Stream EXE...
+:: --noconfirm ensures it overwrites the previous build without asking
+pyinstaller --onefile --windowed --icon=IGTV.jpg --name=24x7Live_Instagram --noconfirm InstagramLiveStreamFolder.py
 
-echo [3/4] Building YouTube Stream EXE...
-cd YTstream
-pyinstaller --onefile --windowed --icon=YouTubeLiveStream.jpg --name=24x7Live_YouTube YouTubeLiveStreamFolder.py
-cd ..
-
-echo [4/4] Signing Executables...
-%SIGNTOOL_PATH% sign /f %PFX_PATH% /p %PFX_PASS% /fd SHA256 /t http://timestamp.digicert.com "IGstream\dist\24x7Live_Instagram.exe"
-%SIGNTOOL_PATH% sign /f %PFX_PATH% /p %PFX_PASS% /fd SHA256 /t http://timestamp.digicert.com "YTstream\dist\24x7Live_YouTube.exe"
+echo [3/3] Signing the Executable for %COMPANY_NAME%...
+%SIGNTOOL_PATH% sign /f %PFX_PATH% /p %PFX_PASS% /fd SHA256 /t http://timestamp.digicert.com "dist\24x7Live_Instagram.exe"
 
 echo.
 echo ---------------------------------------------------
-echo DONE! Your signed EXEs are in their respective /dist folders.
+echo SUCCESS! Your signed Instagram EXE is in the 'dist' folder.
 echo ---------------------------------------------------
 pause
